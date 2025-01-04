6cb3290672837ff7f99851cf715497146811603b
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-04 14:59:59 +0600

    now can get data from db of menu and reviews

--- index.js
+++ index.js
@@ -16,24 +16,44 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const menuCollection = client.db('bistroDB').collection('menu');
+        const reviewCollection = client.db('bistroDB').collection('reviews');
+        
+
+        // menu api's
+        app.get('/menu', async(req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result);
+        });
+
+
+
+        // review api's
+        app.get('/reviews', async(req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        });
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
