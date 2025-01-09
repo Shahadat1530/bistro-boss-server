3b109cd85cceec5c3f4df234faf301183e96916d
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-09 12:40:24 +0600

    user can be delete now

--- index.js
+++ index.js
@@ -34,17 +34,28 @@ async function run() {
         const cartCollection = client.db('bistroDB').collection('carts');
 
         // user api's
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        });
+
         app.post('/users', async (req, res) => {
             const user = req.body;
 
             const query = { email: user.email };
             const exitingUser = await userCollection.findOne(query);
-            if(exitingUser) {
-                return res.send({massage:'User already exists', insertedId: null})
+            if (exitingUser) {
+                return res.send({ massage: 'User already exists', insertedId: null })
             }
 
-
             const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+        app.delete('/users/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id)};
+            const result =  await userCollection.deleteOne(query);
             res.send(result);
         });
 
