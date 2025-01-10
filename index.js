c3022b6ac562766fa6e869a09f89f65deea1f337
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-10 19:29:07 +0600

    checking admin or not.. and giving status

--- index.js
+++ index.js
@@ -44,13 +44,19 @@ async function run() {
 
         // middlewares
         const verifyToken = (req, res, next) => {
+            // console.log('inside the verify', req.headers.authorization);
             if (!req.headers.authorization) {
                 return res.status(401).send({ message: 'forbidden access' });
             }
 
             const token = req.headers.authorization.split(' ')[1]
-
-            // next();
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'forbidden access' });
+                }
+                req.decoded = decoded;
+                next();
+            })
         }
 
 
@@ -58,6 +64,22 @@ async function run() {
         app.get('/users', verifyToken, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
+        });
+
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'unauthorize access' });
+            };
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            };
+            res.send({ admin });
+
         });
 
         app.post('/users', async (req, res) => {
