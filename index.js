1bea755defe1ec463f904c46d34ec94c13db0a17
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-01-13 08:53:27 +0600

    now can be store menu, also secured it

--- index.js
+++ index.js
@@ -133,6 +133,11 @@ async function run() {
             res.send(result);
         });
 
+        app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
+            const item = req.body;
+            const result = await menuCollection.insertOne(item);
+            res.send(result);
+        });
 
         // review api's
         app.get('/reviews', async (req, res) => {
